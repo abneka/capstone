279f83e16b6d454abf9926dbec3be0899a954dd5
abneka <anekahiwot@gmail.com>
2024-05-11 12:03:48 +0300

    book endpoints

---
+++ _data/book.js
@@ -0,0 +1,107 @@
+[
+    {
+        "_id": "5f7a514b5d2c12c7449be042",
+        "title": "To Kill a Mockingbird",
+        "description": "A novel by Harper Lee set in the American South during the 1930s, dealing with racial injustice and the loss of innocence.",
+        "price": 10.99,
+        "published_year": 1960
+    },
+    {
+        "_id": "5f7a514b5d2c12c7449be043",
+        "title": "1984",
+        "description": "A dystopian novel by George Orwell portraying a totalitarian regime where individuality and freedom are suppressed.",
+        "price": 12.99,
+        "published_year": 1949
+    },
+    {
+        "_id": "5f7a514b5d2c12c7449be044",
+        "title": "The Great Gatsby",
+        "description": "A novel by F. Scott Fitzgerald set in the 1920s, depicting the glamorous but ultimately tragic lives of wealthy New Yorkers.",
+        "price": 11.99,
+        "published_year": 1925
+    },
+    {
+        "_id": "5f7a514b5d2c12c7449be045",
+        "title": "Harry Potter and the Philosopher's Stone",
+        "description": "The first book in the Harry Potter series by J.K. Rowling, following the journey of a young wizard, Harry Potter, at Hogwarts School of Witchcraft and Wizardry.",
+        "price": 14.99,
+        "published_year": 1997
+    },
+    {
+        "_id": "5f7a514b5d2c12c7449be046",
+        "title": "The Catcher in the Rye",
+        "description": "A novel by J.D. Salinger narrated by a disaffected teenager, Holden Caulfield, who struggles with the phoniness of the adult world.",
+        "price": 9.99,
+        "published_year": 1951
+    },
+    {
+        "_id": "5f7a514b5d2c12c7449be047",
+        "title": "To Kill a Mockingbird",
+        "description": "A novel by Harper Lee set in the American South during the 1930s, dealing with racial injustice and the loss of innocence.",
+        "price": 10.99,
+        "published_year": 1960
+    },
+    {
+        "_id": "5f7a514b5d2c12c7449be048",
+        "title": "1984",
+        "description": "A dystopian novel by George Orwell portraying a totalitarian regime where individuality and freedom are suppressed.",
+        "price": 12.99,
+        "published_year": 1949
+    },
+    {
+        "_id": "5f7a514b5d2c12c7449be049",
+        "title": "The Great Gatsby",
+        "description": "A novel by F. Scott Fitzgerald set in the 1920s, depicting the glamorous but ultimately tragic lives of wealthy New Yorkers.",
+        "price": 11.99,
+        "published_year": 1925
+    },
+    {
+        "_id": "5f7a514b5d2c12c7449be050",
+        "title": "Harry Potter and the Philosopher's Stone",
+        "description": "The first book in the Harry Potter series by J.K. Rowling, following the journey of a young wizard, Harry Potter, at Hogwarts School of Witchcraft and Wizardry.",
+        "price": 14.99,
+        "published_year": 1997
+    },
+    {
+        "_id": "5f7a514b5d2c12c7449be051",
+        "title": "The Catcher in the Rye",
+        "description": "A novel by J.D. Salinger narrated by a disaffected teenager, Holden Caulfield, who struggles with the phoniness of the adult world.",
+        "price": 9.99,
+        "published_year": 1951
+    },
+    {
+        "_id": "5f7a514b5d2c12c7449be052",
+        "title": "To Kill a Mockingbird",
+        "description": "A novel by Harper Lee set in the American South during the 1930s, dealing with racial injustice and the loss of innocence.",
+        "price": 10.99,
+        "published_year": 1960
+    },
+    {
+        "_id": "5f7a514b5d2c12c7449be053",
+        "title": "1984",
+        "description": "A dystopian novel by George Orwell portraying a totalitarian regime where individuality and freedom are suppressed.",
+        "price": 12.99,
+        "published_year": 1949
+    },
+    {
+        "_id": "5f7a514b5d2c12c7449be054",
+        "title": "The Great Gatsby",
+        "description": "A novel by F. Scott Fitzgerald set in the 1920s, depicting the glamorous but ultimately tragic lives of wealthy New Yorkers.",
+        "price": 11.99,
+        "published_year": 1925
+    },
+    {
+        "_id": "5f7a514b5d2c12c7449be055",
+        "title": "Harry Potter and the Philosopher's Stone",
+        "description": "The first book in the Harry Potter series by J.K. Rowling, following the journey of a young wizard, Harry Potter, at Hogwarts School of Witchcraft and Wizardry.",
+        "price": 14.99,
+        "published_year": 1997
+    },
+    {
+        "_id": "5f7a514b5d2c12c7449be056",
+        "title": "The Catcher in the Rye",
+        "description": "A novel by J.D. Salinger narrated by a disaffected teenager, Holden Caulfield, who struggles with the phoniness of the adult world.",
+        "price": 9.99,
+        "published_year": 1951
+    }
+]
